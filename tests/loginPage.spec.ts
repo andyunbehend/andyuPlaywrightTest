04ed0e9d9b30b46a24ce829bb5e9d282efdb7e3b
aunbehend <67622525+aunbehend@users.noreply.github.com>
2024-04-13 12:59:48 -0600

    - added log out button test

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -2,14 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
-  await expect(page).toHaveTitle(/The Internet/);
+  await expect(page).toHaveTitle("The Internet");
 });
 
 test('Login', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
-    await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill('tomsmith');
-    await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill('SuperSecretPassword!');
     await page.getByLabel('Password').press('Enter');
     await expect(page.getByText('You logged into a secure area')).toBeVisible();
@@ -18,9 +16,7 @@ test('Login', async ({ page }) => {
 
   test('Invalid Username', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
-    await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill('burrito');
-    await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill('superSecretPassword!');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Your username is invalid!')).toBeVisible();
@@ -28,10 +24,19 @@ test('Login', async ({ page }) => {
 
   test('Invalid Password', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
-    await page.locator('div').filter({ hasText: 'Username' }).nth(4).click();
     await page.getByLabel('Username').fill('tomsmith');
     await page.getByLabel('Password').fill('Burrito1982');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Your password is invalid!')).toBeVisible();
     
+  });
+
+  test('log out button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+    await page.getByLabel('Username').fill('tomsmith');
+    await page.getByLabel('Password').fill('SuperSecretPassword!');
+    await page.getByLabel('Password').press('Enter');
+    await expect(page.getByText('You logged into a secure area')).toBeVisible();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page.getByLabel('Username')).toBeVisible(); 
   });
